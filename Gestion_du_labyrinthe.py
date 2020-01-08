3c97f7b953838818e501129b81a6dedb4a94f03b
Votre nom ou pseudo <votre@email.com>
2020-01-08 12:08:42 +0100

    Récupérer le fichier de dessin du labyrinthe

--- Gestion_du_labyrinthe.py
+++ Gestion_du_labyrinthe.py
@@ -1,10 +1,31 @@
+import pickle
+
 class Gestion_labyritnhe:
+
+	{
+(x, y): "mur",
+(x, y): "chemin",
+(x, y): "McGyver",
+(x, y): "Gardien",
+	}
+
+	with open('Dessin_du_labyrinthe.txt', 'wb') as fichier:
+		mon_pickler = pickle.Pickler(fichier)
+		mon_pickler.dump(score)
+			
+
+class MacGyver:
 
 
 	def __init__(self):
+		self.nom = "MacGyver"
 
 
 
+class Gardien:
 
-		def transformation_en_dico():
-			
+
+	def __init__(self):
+		self.nom = "Gardien"
+
+
